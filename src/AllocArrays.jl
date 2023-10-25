a8cf07f5c5386f2aede2feac04f4b70800de6c58
Eric Hanson <5846501+ericphanson@users.noreply.github.com>
2023-10-26 01:18:20 +0200

    precompile

--- src/AllocArrays.jl
+++ src/AllocArrays.jl
@@ -3,6 +3,7 @@ module AllocArrays
 using ScopedValues: ScopedValue, with
 using Bumper
 using ConcurrentUtilities
+using PrecompileTools
 
 # Two array types
 export AllocArray
@@ -22,5 +23,19 @@ include("CheckedAllocArray.jl")
 include("alloc_interface.jl")
 
 const CURRENT_ALLOCATOR = ScopedValue{Allocator}(DEFAULT_ALLOCATOR)
+
+# PrecompileTools workload
+@setup_workload begin
+    @compile_workload begin
+        b = BumperAllocator(2^10) # 1 KiB
+        a = AllocArray([1])
+        c = CheckedAllocArray(a, MemValid(true))
+        with_allocator(b) do
+            similar(a) .= 1
+            similar(c) .= 1
+            reset!(b)
+        end
+    end
+end
 
 end # module
