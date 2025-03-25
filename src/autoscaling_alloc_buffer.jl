83459a4f330f945d11caf68b03a74230b5b6ea7e
Eric Hanson <5846501+ericphanson@users.noreply.github.com>
2025-03-25 11:24:52 +0100

    Update src/autoscaling_alloc_buffer.jl

--- src/autoscaling_alloc_buffer.jl
+++ src/autoscaling_alloc_buffer.jl
@@ -21,7 +21,7 @@ This means:
 
 - `AutoscalingAllocBuffer` does not run out of memory (unlike `AllocBuffer`), since new memory will allocated on-demand when necessary
 - for repeated runs of the same size, a single contiguous buffer will be used, which should approximately match the performance of a tuned `AllocBuffer`
-- `AutoscalingAllocBuffer` can reuse allocated memory between runs like `AllocBuffer`, but without potentially OOMing like `AutoscalingAllocBuffer`. Additionally, `AutoscalingAllocBuffer` separately tracks the memory used by the main buffer vs the additional buffers allocated dynamically, so small unexpected additional allocations don't double the memory consumption (unlike a second slab being allocated).
+- `AutoscalingAllocBuffer` can reuse allocated memory between runs like `AllocBuffer`, but with safety from overrunning a fixed buffer size and OOMing like `SlabBuffer`. Additionally, `AutoscalingAllocBuffer` separately tracks the memory used by the main buffer vs the additional buffers allocated dynamically, so small unexpected additional allocations don't double the memory consumption (unlike a second slab being allocated).
 
 
 !!! note
